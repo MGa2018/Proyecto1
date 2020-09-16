c499c852ba8ff586492aab91e8ae8ed9d8d5e4c1
MGa2018 <mgsalina2016@gmail.com>
2020-09-16 20:46:48 -0300

    Cambios hechos4

--- Prueba.java
+++ Prueba.java
@@ -5,7 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Ciudad c = new Ciudad("Bahia");
+		Ciudad c = new Ciudad("Huanguelen");
 	}
 
 }
