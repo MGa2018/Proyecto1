d7e079e4fe94706234e0500ac57382e12ade4af6
MGa2018 <42505859+MGa2018@users.noreply.github.com>
2020-09-16 22:43:56 -0300

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -5,7 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Ciudad c = new Ciudad("Bahía Blanca");
+		Ciudad c = new Ciudad("Huanguelen");
 	}
 
 }
